820ca49f61eccd8732d49c715579329f7670b65f
yayalew <90276673+yayalew@users.noreply.github.com>
2025-07-10 10:26:09 -0400

    add emitter looping

--- fnirs/src/nirs/nirscontrolform.cpp
+++ fnirs/src/nirs/nirscontrolform.cpp
@@ -7,7 +7,7 @@
 
 NirsControlForm::NirsControlForm(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::NirsControlForm), m_taskHandleIllumination(), m_analogView(0), m_analogInput(0),
+    , ui(new Ui::NirsControlForm), m_taskHandleIllumination(), m_taskHandleDigitalOut(), m_analogView(0), m_analogInput(0),
       m_bSaveData(false), m_dataSaverAnalogInputs(0)
 {
     ui->setupUi(this);
@@ -19,19 +19,14 @@ NirsControlForm::NirsControlForm(QWidget *parent)
 
     m_viewTimer = new QTimer();
 
-    connect(ui->pushButton_start,SIGNAL(clicked()),this,SLOT(startAcquisition()));
-    connect(ui->pushButton_stop,SIGNAL(clicked()),this,SLOT(stopAcquisition()));
-    connect(ui->checkBox_saveData,SIGNAL(clicked(bool)),this,SLOT(saveData(bool)));
+    connect(ui->pushButton_start, SIGNAL(clicked()), this, SLOT(startAcquisition()));
+    connect(ui->pushButton_stop, SIGNAL(clicked()), this, SLOT(stopAcquisition()));
+    connect(ui->checkBox_saveData, SIGNAL(clicked(bool)), this, SLOT(saveData(bool)));
+    connect(m_viewTimer, SIGNAL(timeout()), this, SLOT(updateDigitalOutput()));
 
     m_saveDir = QDir::home();
     ui->label_savedir->setText(m_saveDir.absolutePath());
-    connect(ui->pushButton_saveDirectory,SIGNAL(clicked()),this,SLOT(setSaveDir()));
-    //    //  Here I am looking for plugins assuming there will only be one camera, eventually we can correct if we mix cameras.
-
-
-
-
-
+    connect(ui->pushButton_saveDirectory, SIGNAL(clicked()), this, SLOT(setSaveDir()));
 }
 
 NirsControlForm::~NirsControlForm()
@@ -41,9 +36,8 @@ NirsControlForm::~NirsControlForm()
 
 void NirsControlForm::startAcquisition()
 {
-
     unsigned int acq_rate = ui->lineEdit_frameRate->text().toUInt();
-    m_analogView = new AnalogViewer(acq_rate,&m_hardwareSettings);
+    m_analogView = new AnalogViewer(acq_rate, &m_hardwareSettings);
     m_analogView->show();
 
     try {
@@ -55,67 +49,70 @@ void NirsControlForm::startAcquisition()
         msg.exec();
         exit(-1);
     }
-    //Pass sampling rate to analoginput object, KP
-    //m_analogInput->set_AnalogSamplingRate((int) acq_rate);
-
 
     // Saving config
-    if(m_bSaveData)
+    if (m_bSaveData)
     {
         m_saveName = ui->lineEdit_datasetName->text();
         m_saveDir.setPath(ui->label_savedir->text());
 
-        // Start Analog inputs, saving will be synchronized since camera triggers are saved.
-        m_dataSaverAnalogInputs = new Float64DataSaver(16,acq_rate,0,256,"nirs");
+        m_dataSaverAnalogInputs = new Float64DataSaver(16, acq_rate, 0, 256, "nirs");
         m_dataSaverAnalogInputs->setDatasetName(m_saveName);
         m_dataSaverAnalogInputs->setDatasetPath(m_saveDir.absolutePath());
         m_analogInput->SetDataSaver(m_dataSaverAnalogInputs);
         m_dataSaverAnalogInputs->startSaving();
-
     }
 
+    // Digital output config for 20 emitters
+    const int numEmitters = 20;
+    QVector<uInt32> digitalPatterns(numEmitters);
+    for (int i = 0; i < numEmitters; ++i) {
+        digitalPatterns[i] = (1U << i); // Bitmask: 0b00000001, 0b00000010, ..., 0b10000000...
+    }
+    m_currentEmitterIndex = 0;
+
+    try {
+        // Create digital output task for 20 lines (e.g., Dev1/port0/line0:19)
+        DAQmxErrChk(DAQmxCreateTask("DigitalOutput", &m_taskHandleDigitalOut));
+        DAQmxErrChk(DAQmxCreateDOChan(m_taskHandleDigitalOut, "/Dev1/port0/line0:19", "", DAQmx_Val_ChanForAllLines));
+        DAQmxErrChk(DAQmxCfgSampClkTiming(m_taskHandleDigitalOut, "/Dev1/ai/SampleClock", acq_rate, DAQmx_Val_Rising, DAQmx_Val_ContSamps, 1));
+        // Write initial digital pattern
+        DAQmxErrChk(DAQmxWriteDigitalU32(m_taskHandleDigitalOut, 1, 0, 10.0, DAQmx_Val_GroupByChannel, &digitalPatterns[m_currentEmitterIndex], nullptr, nullptr));
+    } catch (DAQException& e) {
+        e.show();
+        exit(-1);
+    }
 
     // Illumination config
-    int nSamplesOn=100*acq_rate;
+    int nSamplesOn = 100 * acq_rate;
     QVector<uInt32> IllumVect;
-    IllumVect.fill(1,nSamplesOn-1);
+    IllumVect.fill(1, nSamplesOn - 1);
     try {
-        //Lights output, synchronized and triggered on camera clock
-        DAQmxErrChk(DAQmxCreateTask("Illumination",&m_taskHandleIllumination));
-        DAQmxErrChk(DAQmxCreateDOChan(m_taskHandleIllumination,"/Dev1/port0","",DAQmx_Val_ChanForAllLines));
-        // Turn on light according to vector
+        DAQmxErrChk(DAQmxCreateTask("Illumination", &m_taskHandleIllumination));
+        DAQmxErrChk(DAQmxCreateDOChan(m_taskHandleIllumination, "/Dev1/port0", "", DAQmx_Val_ChanForAllLines));
         DAQmxErrChk(DAQmxCfgOutputBuffer(m_taskHandleIllumination, IllumVect.length()));
-        // Base frequency is 100 * framerate, triggered on camera clock and re-triggered. Have to make sure we are done prior to camera stop.
-        DAQmxErrChk(DAQmxCfgSampClkTiming(m_taskHandleIllumination, nullptr, 10000, DAQmx_Val_Rising, DAQmx_Val_FiniteSamps, nSamplesOn-1));
+        DAQmxErrChk(DAQmxCfgSampClkTiming(m_taskHandleIllumination, nullptr, 10000, DAQmx_Val_Rising, DAQmx_Val_FiniteSamps, nSamplesOn - 1));
         DAQmxErrChk(DAQmxCfgDigEdgeStartTrig(m_taskHandleIllumination, "/Dev1/aiStartTrigger", DAQmx_Val_Rising));
         DAQmxErrChk(DAQmxWriteDigitalU32(m_taskHandleIllumination, IllumVect.length(), 0, 10.0, DAQmx_Val_GroupByChannel, IllumVect.data(), nullptr, nullptr));
-    }
-    catch (DAQException& e)
-    {
+    } catch (DAQException& e) {
         e.show();
         exit(-1);
     }
 
     // Camera clock
-    DAQmxErrChk(DAQmxCreateTask("Clock",&m_taskHandleClock));
-    DAQmxErrChk(DAQmxCreateCOPulseChanFreq(m_taskHandleClock,"/Dev1/ctr0","",DAQmx_Val_Hz,DAQmx_Val_Low,0.0,(double)acq_rate,0.5));
+    DAQmxErrChk(DAQmxCreateTask("Clock", &m_taskHandleClock));
+    DAQmxErrChk(DAQmxCreateCOPulseChanFreq(m_taskHandleClock, "/Dev1/ctr0", "", DAQmx_Val_Hz, DAQmx_Val_Low, 0.0, (double)acq_rate, 0.5));
 
     m_analogInput->setAnalogViewer(m_analogView);
- //   connect(m_analogView, SIGNAL(aboutToQuit()), this, SLOT(analogViewClosed()));
 
     // Start everything in the right order
-    try
-    {
-        // Camera and analog IN are trigged on PFI12 from digital out clock, start it first since it won't record until
-        // it receives a trigger
+    try {
+        DAQmxErrChk(DAQmxStartTask(m_taskHandleDigitalOut));
         DAQmxErrChk(DAQmxStartTask(m_taskHandleIllumination));
         m_analogInput->Start(false);
-        m_viewTimer->start(30);
+        m_viewTimer->start(1000 / acq_rate); // Update at acquisition rate
         DAQmxErrChk(DAQmxStartTask(m_taskHandleClock));
-
-    }
-    catch (DAQException& e)
-    {
+    } catch (DAQException& e) {
         e.show();
         exit(-1);
     }
@@ -125,19 +122,16 @@ void NirsControlForm::stopAcquisition()
 {
     m_analogInput->Stop();
 
-    if(m_dataSaverAnalogInputs){
+    if (m_dataSaverAnalogInputs) {
         m_dataSaverAnalogInputs->stopSaving();
         delete m_dataSaverAnalogInputs;
-        m_dataSaverAnalogInputs=0;
+        m_dataSaverAnalogInputs = 0;
     }
-//    disconnect(m_analogView, SIGNAL(aboutToQuit()), this, SLOT(analogViewClosed()));
     m_analogView->close();
     delete m_analogView;
     m_analogView = nullptr;
 
-
     m_analogInput->resetDataSaver();
-
 
     m_viewTimer->stop();
 
@@ -145,25 +139,32 @@ void NirsControlForm::stopAcquisition()
     DAQmxClearTask(m_taskHandleClock);
     DAQmxStopTask(m_taskHandleIllumination);
     DAQmxClearTask(m_taskHandleIllumination);
+    DAQmxStopTask(m_taskHandleDigitalOut);
+    DAQmxClearTask(m_taskHandleDigitalOut);
     delete m_analogInput;
-
-
 }
 
+void NirsControlForm::updateDigitalOutput()
+{
+    const int numEmitters = 20;
+    m_currentEmitterIndex = (m_currentEmitterIndex + 1) % numEmitters;
+    uInt32 digitalPattern = (1U << m_currentEmitterIndex);
+    try {
+        DAQmxErrChk(DAQmxWriteDigitalU32(m_taskHandleDigitalOut, 1, 0, 10.0, DAQmx_Val_GroupByChannel, &digitalPattern, nullptr, nullptr));
+    } catch (DAQException& e) {
+        e.show();
+        stopAcquisition();
+    }
+}
 
 void NirsControlForm::saveData(bool flag)
 {
     m_bSaveData = flag;
 }
 
-void NirsControlForm::analogViewClosed()
-{
-    //Closed by user using X
-}
-
 void NirsControlForm::setSaveDir()
 {
-    QString dataDir = QFileDialog::getExistingDirectory(this, tr("choose Directory"),
+    QString dataDir = QFileDialog::getExistingDirectory(this, tr("Choose Directory"),
                                                 m_saveDir.absolutePath(),
                                                 QFileDialog::ShowDirsOnly
                                                 | QFileDialog::DontResolveSymlinks);
--- fnirs/src/nirs/nirscontrolform.h
+++ fnirs/src/nirs/nirscontrolform.h
@@ -2,49 +2,44 @@
 #define NIRSCONTROLFORM_H
 
 #include <QMainWindow>
+#include <QDir>
 #include <QTimer>
-#include "analoginput.h"
 #include "analogviewer.h"
+#include "analoginput.h"
 #include "float64datasaver.h"
-#include "hardwaresettings.h"
-#include "NIDAQmx.h"
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class NirsControlForm; }
-QT_END_NAMESPACE
+namespace Ui {
+class NirsControlForm;
+}
 
 class NirsControlForm : public QMainWindow
 {
     Q_OBJECT
-
 public:
-    NirsControlForm(QWidget *parent = nullptr);
+    explicit NirsControlForm(QWidget *parent = nullptr);
     ~NirsControlForm();
 
 private slots:
     void startAcquisition();
     void stopAcquisition();
-    void saveData(bool);
+    void saveData(bool flag);
     void setSaveDir();
-    void analogViewClosed();
+    void updateDigitalOutput();
 
 private:
     Ui::NirsControlForm *ui;
-    AnalogInput* m_analogInput;
-    AnalogViewer* m_analogView;
-    QString m_saveName;
-    QDir m_saveDir;
-    bool m_isUserStop;
-    QRect m_ROI;
-    Float64DataSaver* m_dataSaverAnalogInputs;
-    TaskHandle m_taskHandleIllumination;
+    TaskHandle m_taskHandleDigitalOut;
     TaskHandle m_taskHandleClock;
-    int m_error;
-    HardwareSettings m_hardwareSettings;
-    QTimer* m_viewTimer;
-
-
-    bool m_bShowCameraROI;
+    AnalogViewer* m_analogView;
+    AnalogInput* m_analogInput;
     bool m_bSaveData;
+    Float64DataSaver* m_dataSaverAnalogInputs;
+    QDir m_saveDir;
+    QString m_saveName;
+    QTimer* m_viewTimer;
+    int m_currentEmitterIndex;
+    int32 m_error;
+    // Assume m_hardwareSettings is defined elsewhere
 };
+
 #endif // NIRSCONTROLFORM_H
