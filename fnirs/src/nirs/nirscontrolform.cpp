0daa912dbded7195c8b19392daf4f34930b1c7e4
Yeab Ayalew <90276673+yayalew@users.noreply.github.com>
2025-07-11 12:35:04 -0400

    Update nirscontrolform.cpp - implement 24 emitters

--- fnirs/src/nirs/nirscontrolform.cpp
+++ fnirs/src/nirs/nirscontrolform.cpp
@@ -7,7 +7,7 @@
 
 NirsControlForm::NirsControlForm(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::NirsControlForm), m_taskHandleIllumination(), m_taskHandleDigitalOut(), m_analogView(0), m_analogInput(0),
+    , ui(new Ui::NirsControlForm), m_taskHandleIllumination(0), m_analogView(0), m_analogInput(0),
       m_bSaveData(false), m_dataSaverAnalogInputs(0)
 {
     ui->setupUi(this);
@@ -18,15 +18,16 @@ NirsControlForm::NirsControlForm(QWidget *parent)
     ui->lineEdit_datasetName->setValidator(validator);
 
     m_viewTimer = new QTimer();
+    m_viewTimer->setTimerType(Qt::PreciseTimer);
 
     connect(ui->pushButton_start, SIGNAL(clicked()), this, SLOT(startAcquisition()));
     connect(ui->pushButton_stop, SIGNAL(clicked()), this, SLOT(stopAcquisition()));
     connect(ui->checkBox_saveData, SIGNAL(clicked(bool)), this, SLOT(saveData(bool)));
-    connect(m_viewTimer, SIGNAL(timeout()), this, SLOT(updateDigitalOutput()));
+    connect(ui->pushButton_saveDirectory, SIGNAL(clicked()), this, SLOT(setSaveDir()));
+    connect(m_viewTimer, SIGNAL(timeout()), this, SLOT(updateIllumination()));
 
     m_saveDir = QDir::home();
     ui->label_savedir->setText(m_saveDir.absolutePath());
-    connect(ui->pushButton_saveDirectory, SIGNAL(clicked()), this, SLOT(setSaveDir()));
 }
 
 NirsControlForm::~NirsControlForm()
@@ -42,10 +43,12 @@ void NirsControlForm::startAcquisition()
 
     try {
         m_analogInput = new AnalogInput(acq_rate);
-    } catch (DAQException&) {
+    } catch (DAQException& e) {
+        char errBuff[2048] = {'\0'};
+        DAQmxGetErrorString(e.getError(), errBuff, 2048);
         QMessageBox msg;
-        msg.setWindowTitle("Error");
-        msg.setText(QString("Could not find the acquisition card, is the power on on the card, is it plugged?"));
+        msg.setWindowTitle("DAQmx Error");
+        msg.setText(QString("Could not initialize analog input: %1").arg(errBuff));
         msg.exec();
         exit(-1);
     }
@@ -63,57 +66,52 @@ void NirsControlForm::startAcquisition()
         m_dataSaverAnalogInputs->startSaving();
     }
 
-    // Digital output config for 20 emitters
-    const int numEmitters = 20;
-    QVector<uInt32> digitalPatterns(numEmitters);
+    // Illumination config for 20 emitters across port0/0:7, port1/0:7, port2/0:3
+    const int numEmitters = 24;
+    m_illumStates.resize(numEmitters);
     for (int i = 0; i < numEmitters; ++i) {
-        digitalPatterns[i] = (1U << i); // Bitmask: 0b00000001, 0b00000010, ..., 0b10000000...
+        m_illumStates[i].resize(numEmitters);
+        m_illumStates[i].fill(0); // All lines LOW
+        m_illumStates[i][i] = 1;  // Set one line HIGH
     }
     m_currentEmitterIndex = 0;
 
     try {
-        // Create digital output task for 20 lines (e.g., Dev1/port0/line0:19)
-        DAQmxErrChk(DAQmxCreateTask("DigitalOutput", &m_taskHandleDigitalOut));
-        DAQmxErrChk(DAQmxCreateDOChan(m_taskHandleDigitalOut, "/Dev1/port0/line0:19", "", DAQmx_Val_ChanForAllLines));
-        DAQmxErrChk(DAQmxCfgSampClkTiming(m_taskHandleDigitalOut, "/Dev1/ai/SampleClock", acq_rate, DAQmx_Val_Rising, DAQmx_Val_ContSamps, 1));
-        // Write initial digital pattern
-        DAQmxErrChk(DAQmxWriteDigitalU32(m_taskHandleDigitalOut, 1, 0, 10.0, DAQmx_Val_GroupByChannel, &digitalPatterns[m_currentEmitterIndex], nullptr, nullptr));
-    } catch (DAQException& e) {
-        e.show();
-        exit(-1);
-    }
-
-    // Illumination config
-    int nSamplesOn = 100 * acq_rate;
-    QVector<uInt32> IllumVect;
-    IllumVect.fill(1, nSamplesOn - 1);
-    try {
+        // Ensure no residual task exists
+        if (m_taskHandleIllumination) {
+            DAQmxStopTask(m_taskHandleIllumination);
+            DAQmxClearTask(m_taskHandleIllumination);
+        }
+        // Create digital output task for 20 emitters (software-timed)
         DAQmxErrChk(DAQmxCreateTask("Illumination", &m_taskHandleIllumination));
-        DAQmxErrChk(DAQmxCreateDOChan(m_taskHandleIllumination, "/Dev1/port0", "", DAQmx_Val_ChanForAllLines));
-        DAQmxErrChk(DAQmxCfgOutputBuffer(m_taskHandleIllumination, IllumVect.length()));
-        DAQmxErrChk(DAQmxCfgSampClkTiming(m_taskHandleIllumination, nullptr, 10000, DAQmx_Val_Rising, DAQmx_Val_FiniteSamps, nSamplesOn - 1));
-        DAQmxErrChk(DAQmxCfgDigEdgeStartTrig(m_taskHandleIllumination, "/Dev1/aiStartTrigger", DAQmx_Val_Rising));
-        DAQmxErrChk(DAQmxWriteDigitalU32(m_taskHandleIllumination, IllumVect.length(), 0, 10.0, DAQmx_Val_GroupByChannel, IllumVect.data(), nullptr, nullptr));
+        DAQmxErrChk(DAQmxCreateDOChan(m_taskHandleIllumination, "/Dev2/port0/line0:7,/Dev2/port1/line0:7,/Dev2/port2/line0:7", "", DAQmx_Val_ChanPerLine));
+        // Write initial digital pattern (software-timed)
+        DAQmxErrChk(DAQmxWriteDigitalLines(m_taskHandleIllumination, 1, 1, 10.0, DAQmx_Val_GroupByChannel, m_illumStates[m_currentEmitterIndex].data(), nullptr, nullptr));
     } catch (DAQException& e) {
-        e.show();
+        char errBuff[2048] = {'\0'};
+        DAQmxGetErrorString(e.getError(), errBuff, 2048);
+        QMessageBox msg;
+        msg.setWindowTitle("DAQmx Error");
+        msg.setText(QString("Error in illumination setup: %1").arg(errBuff));
+        msg.exec();
         exit(-1);
     }
-
-    // Camera clock
-    DAQmxErrChk(DAQmxCreateTask("Clock", &m_taskHandleClock));
-    DAQmxErrChk(DAQmxCreateCOPulseChanFreq(m_taskHandleClock, "/Dev1/ctr0", "", DAQmx_Val_Hz, DAQmx_Val_Low, 0.0, (double)acq_rate, 0.5));
 
     m_analogInput->setAnalogViewer(m_analogView);
 
     // Start everything in the right order
     try {
-        DAQmxErrChk(DAQmxStartTask(m_taskHandleDigitalOut));
         DAQmxErrChk(DAQmxStartTask(m_taskHandleIllumination));
         m_analogInput->Start(false);
-        m_viewTimer->start(1000 / acq_rate); // Update at acquisition rate
-        DAQmxErrChk(DAQmxStartTask(m_taskHandleClock));
+
+        m_viewTimer->start(1000 / acq_rate); // Update at acquisition rate (e.g., 50 ms for 20 Hz)
     } catch (DAQException& e) {
-        e.show();
+        char errBuff[2048] = {'\0'};
+        DAQmxGetErrorString(e.getError(), errBuff, 2048);
+        QMessageBox msg;
+        msg.setWindowTitle("DAQmx Error");
+        msg.setText(QString("Error starting tasks: %1").arg(errBuff));
+        msg.exec();
         exit(-1);
     }
 }
@@ -135,26 +133,11 @@ void NirsControlForm::stopAcquisition()
 
     m_viewTimer->stop();
 
-    DAQmxStopTask(m_taskHandleClock);
-    DAQmxClearTask(m_taskHandleClock);
-    DAQmxStopTask(m_taskHandleIllumination);
-    DAQmxClearTask(m_taskHandleIllumination);
-    DAQmxStopTask(m_taskHandleDigitalOut);
-    DAQmxClearTask(m_taskHandleDigitalOut);
-    delete m_analogInput;
-}
-
-void NirsControlForm::updateDigitalOutput()
-{
-    const int numEmitters = 20;
-    m_currentEmitterIndex = (m_currentEmitterIndex + 1) % numEmitters;
-    uInt32 digitalPattern = (1U << m_currentEmitterIndex);
-    try {
-        DAQmxErrChk(DAQmxWriteDigitalU32(m_taskHandleDigitalOut, 1, 0, 10.0, DAQmx_Val_GroupByChannel, &digitalPattern, nullptr, nullptr));
-    } catch (DAQException& e) {
-        e.show();
-        stopAcquisition();
+    if (m_taskHandleIllumination) {
+        DAQmxStopTask(m_taskHandleIllumination);
+        DAQmxClearTask(m_taskHandleIllumination);
     }
+    delete m_analogInput;
 }
 
 void NirsControlForm::saveData(bool flag)
@@ -171,3 +154,28 @@ void NirsControlForm::setSaveDir()
     m_saveDir.setPath(dataDir);
     ui->label_savedir->setText(m_saveDir.absolutePath());
 }
+
+void NirsControlForm::updateIllumination()
+{
+    const int numEmitters = 24;
+    m_currentEmitterIndex = (m_currentEmitterIndex + 1) % numEmitters;
+    try {
+        DAQmxErrChk(DAQmxWriteDigitalLines(m_taskHandleIllumination, 1, 1, 10.0, DAQmx_Val_GroupByChannel, m_illumStates[m_currentEmitterIndex].data(), nullptr, nullptr));
+    } catch (DAQException& e) {
+        char errBuff[2048] = {'\0'};
+        DAQmxGetErrorString(e.getError(), errBuff, 2048);
+        QMessageBox msg;
+        msg.setWindowTitle("DAQmx Error");
+        msg.setText(QString("Error updating illumination: %1").arg(errBuff));
+        msg.exec();
+        stopAcquisition();
+    }
+}
+
+void NirsControlForm::analogViewClosed()
+{
+    //Closed by user using X
+}
+
+
+
